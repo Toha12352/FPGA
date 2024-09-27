c78b7ad0e9ed23ed41f64ca70667bde358ee5e6d
MD ABU TOHA <mdabutoha2002@gmail.com>
2024-09-27 05:33:19 +0530

    Create FULL_ADDER.v

---
+++ FULL_ADDER.v
@@ -0,0 +1,13 @@
+module FULL_ADDER(
+   input A,      // First input bit
+    input B,      // Second input bit
+    input Cin,    // Carry-in bit
+    output Sum,   // Sum output
+    output Cout   // Carry-out output
+);
+  wire w1 ,w2 ,w3;
+    assign Sum = A ^ B ^ Cin;        // XOR operation for sum
+    assign Cout = (A & B) | (Cin & (A ^ B));  // Carry-out calculation
+
+
+endmodule
